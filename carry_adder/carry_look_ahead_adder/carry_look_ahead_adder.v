425725338f1581355dd58fc531e7e0f07c843b58
Vishesh Bindal <visheshbindal2007@gmail.com>
2026-06-18 11:30:06 +0530

    updated

---
+++ carry_adder/carry_look_ahead_adder/carry_look_ahead_adder.v
@@ -0,0 +1,10 @@
+module carry_look_ahead #(
+    parameter n = 16
+)(
+    input [n-1:0] a,b,
+    input cin,
+    output [n-1:0] y,
+    output cout
+);
+
+endmodule
---
+++ carry_adder/carry_skip_adder/carry_skip_adder.v
@@ -0,0 +1,10 @@
+module carry_skip #(
+    parameter n = 16
+)(
+    input [n-1:0] a,b,
+    input cin,
+    output [n-1:0] y,
+    output cout
+);
+
+endmodule
