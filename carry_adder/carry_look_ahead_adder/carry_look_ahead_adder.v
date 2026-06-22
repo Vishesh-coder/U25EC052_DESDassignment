93aa958e590023f0e294d06a3728288e9d79ff65
Vishesh Bindal <visheshbindal2007@gmail.com>
2026-06-22 10:28:50 +0530

    updated

--- carry_adder/carry_look_ahead_adder/carry_look_ahead_adder.v
+++ carry_adder/carry_look_ahead_adder/carry_look_ahead_adder.v
@@ -7,4 +7,22 @@ module carry_look_ahead #(
     output cout
 );
 
+wire [n-1:0] p;
+wire [n-1:0] g;
+wire [n:0] c;
+
+assign c[0] = cin;
+assign p = a ^ b;
+assign g = a & b;
+
+genvar i;
+generate
+    for (i = 0; i < n; i = i + 1) begin
+        assign c[i+1] = g[i] | (p[i] & c[i]);
+    end
+endgenerate
+
+assign cout = c[n];
+assign y = p ^ c[n-1:0];
+
 endmodule
